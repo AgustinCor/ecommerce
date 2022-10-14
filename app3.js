83d88ec9d061e763e8f52c5cd1f80c70a0a975d1
AgustinCor <agustin.cordero@gmail.com>
2022-10-14 13:48:52 -0300

    Updated 2

--- app3.js
+++ app3.js
@@ -3,6 +3,7 @@ const productContainer=document.querySelector(".product-container");
 const addProduct1=document.querySelector(".a-button1");
 const addProduct2=document.querySelector(".a-button2");
 const addProduct3=document.querySelector(".a-button3");
+const checKout=document.querySelector(".checKout");
 
 let productArray=JSON.parse(localStorage.getItem("products")) ?? [];
 
@@ -21,8 +22,12 @@ productContainer.addEventListener("click",(e)=>{
     createProduct();
     console.log(productArray[0].unit);
    }; 
-   
 });
+
+checKout.addEventListener("click", (e)=>{
+  console.log(e.target);
+  chargePorduct();
+})
 
 addProduct1.addEventListener("click",(e)=>{
       productArray.push({id:1 ,nombre:"Buzo1" ,tipo:"hoodie", unit:1, precio:14,img:"featured1.png"});
@@ -41,12 +46,13 @@ addProduct3.addEventListener("click",(e)=>{
 function createProduct(){
     const elements=productArray.map((products)=>{
         return `
-        <div class="products">
+     <img class="aside-hidden hide" src="empty-cart.png">
+      <div class="products">
       <img src=${products.img}>   
         <div class="products-info">
           <h3>Shirts</h3>
           <p class="stock">Stock:10<span>|$24.00</span></p>  
-          <p>Subtotal:<span>$24.00</span></p>
+          <p>Subtotal:<span></span></p>
           <div class="plus-products">
             <a><i class="fa-solid fa-minus"></i></a> 
             <p><span class="units">${products.unit}</span>units</p>
@@ -54,12 +60,14 @@ function createProduct(){
             <a class="minusUnit"><i class="minusUnit fa-solid fa-trash"></i></a>
           </div>
         </div>
-      </div>
-      <img class=${"hide"} src="empty-cart.png">`;
+      </div>`;
     });
-    productContainer.innerHTML=elements.join('');
+    productContainer.innerHTML=elements;
 }
 
+function chargePorduct(){
+  productContainer.innerHTML="";
+}
 
 //const products=document.querySelector('aside');
 //const cartIcon=document.querySelector("#cart-icon");
